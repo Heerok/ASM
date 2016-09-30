361d6b3dcd29ca7db8828254cb564ed1d236a530
Heerok <heerok.jyoti@humaralabs.com>
2016-09-30 15:57:27 +0530

    first commit

---
+++ src/main/java/digitalaxom/asm/MainServer.java
@@ -0,0 +1,26 @@
+package digitalaxom.asm;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
+
+/**
+ * Created by Heerok on 24-09-2016.
+ */
+@SpringBootApplication
+public class MainServer extends SpringBootServletInitializer {
+
+    private static final Logger logger = LoggerFactory.getLogger(MainServer.class);
+
+    public static void main(String[] args) throws Exception{
+        SpringApplication.run(MainServer.class,args);
+    }
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MainServer.class);
+    }
+
+}
