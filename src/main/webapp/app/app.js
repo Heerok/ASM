de2601698a661b06a15b872105863be5466fac85
Heerok <heerok.jyoti@humaralabs.com>
2017-02-28 13:20:11 +0530

    -- google analytics

--- src/main/webapp/app/app.js
+++ src/main/webapp/app/app.js
@@ -2,5 +2,5 @@
 var app = angular.module('app', []);
 
 app.constant('urls', {
-    BASE_API: 'http://asomsattramahasabha.org:8193/asm4/'
+    BASE_API: 'http://ec2-52-37-142-200.us-west-2.compute.amazonaws.com:8080/'
 });
